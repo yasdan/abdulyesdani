c87014374718b323b056d0ca1781047baed1e64c
Abdul Yesdani <yasdanis@gmail.com>
2021-11-01 19:54:17 +0530

    Initial comment

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import{ Route, Switch } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import UserPage from './Pages/UserPage';
+
+export default function App(){
+  return(
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/:id" component={UserPage} />
+    </Switch>
+  )
+}
+
+ 
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
